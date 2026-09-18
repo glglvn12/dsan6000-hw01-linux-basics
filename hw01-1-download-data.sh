d3d2fe0748b6127a92d720ebfa1928ed97ff7b1f
“glglvn12” <glglvn12@gmail.com>
2026-09-18 06:13:31 +0000

    before rm 100gb instance

--- hw01-1-download-data.sh
+++ hw01-1-download-data.sh
@@ -1,3 +1,3 @@
-aws s3 ls s3://dsan6000-wikipedia/hourly/
-aws s3 cp s3://dsan6000-wikipedia/hourly/ data/ --recursive
+aws s3 ls s3://dsan6000-wikipedia/
+aws s3 cp s3://dsan6000-wikipedia/dsan6000-hw01-linux-basics/ data/ --recursive
 ls data/*.csv
